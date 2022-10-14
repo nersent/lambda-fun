546526981d84c618814c3e0d497c62526812689f
xnerhu <xnerhu@gmail.com>
2022-10-14 18:07:56 +0200

    update repeater

--- src/repeater/repeater.ts
+++ src/repeater/repeater.ts
@@ -2,6 +2,8 @@ import { delay } from "../utils";
 import { IRepeater, RepeaterOptions } from "./repeater-types";
 
 export class Repeater implements IRepeater {
+  public currentAttempt = 0;
+
   constructor(public readonly options?: RepeaterOptions) {}
 
   public async execute<T>(fn: (...args: any[]) => T) {
@@ -9,6 +11,8 @@ export class Repeater implements IRepeater {
     if (maxAttempts <= 0) return;
 
     let currentAttempt = 0;
+    this.currentAttempt = currentAttempt;
+
     let result;
     let error;
     let success = false;
@@ -21,29 +25,33 @@ export class Repeater implements IRepeater {
       } catch (err) {
         error = err;
         currentAttempt++;
+        this.currentAttempt = currentAttempt;
         if (this.options?.timeout != null) {
           await delay(this.options.timeout);
         }
       }
     }
 
-    if (error != null && !success) {
-      if (this.options?.onMaxAttemptsExceeded != null) {
-        if (
-          "returnUndefined" in this.options.onMaxAttemptsExceeded &&
-          this.options.onMaxAttemptsExceeded.returnUndefined
-        ) {
-          return undefined;
+    if (currentAttempt > maxAttempts) {
+      if (error != null && !success) {
+        if (this.options?.onMaxAttemptsExceeded != null) {
+          if (
+            "returnUndefined" in this.options.onMaxAttemptsExceeded &&
+            this.options.onMaxAttemptsExceeded.returnUndefined
+          ) {
+            return undefined;
+          }
+          if (
+            "exitProcess" in this.options.onMaxAttemptsExceeded &&
+            this.options.onMaxAttemptsExceeded.exitProcess
+          ) {
+            console.error("Max attempts exceeded");
+            console.error(error);
+            process.exit();
+          }
         }
-        if (
-          "exitProcess" in this.options.onMaxAttemptsExceeded &&
-          this.options.onMaxAttemptsExceeded.exitProcess
-        ) {
-          console.error(error);
-          process.exit();
-        }
+        throw error;
       }
-      throw error;
     }
 
     return result;
